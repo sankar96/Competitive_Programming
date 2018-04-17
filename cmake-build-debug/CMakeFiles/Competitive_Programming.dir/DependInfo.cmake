b0c3f713b11c30ccc0c332b29f926b3f6fa5be9c
sankaranarayanan.g <sankaranarayanan.g@zohocorp.com>
2018-04-17 14:06:44 +0530

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Competitive_Programming.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/sankar-5526/CLionProjects/Competitive_Programming/main.cpp" "/Users/sankar-5526/CLionProjects/Competitive_Programming/cmake-build-debug/CMakeFiles/Competitive_Programming.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Competitive_Programming.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Competitive_Programming.dir/main.cpp.o"
+  "Competitive_Programming.pdb"
+  "Competitive_Programming"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Competitive_Programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
