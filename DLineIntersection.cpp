208ccb1fc126510c7b2ce09b9b4e301e0669719a
sankaranarayanan.g <sankaranarayanan.g@zohocorp.com>
2018-04-24 18:10:04 +0530

    added new files

---
+++ DLineIntersection.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Sankaranarayanan G on 19/04/18.
+//
+
---
+++ test/GTest.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Sankaranarayanan G on 06/04/18.
+//
+
+#include <gtest/gtest.h>
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
